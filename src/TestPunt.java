a434a3992b1abce011d5d1a8daab3ead5bc7b294
cayeym <cf18cayetano.yeste@iesjoandaustria.org>
2019-05-20 19:01:19 +0200

    Resolt exercici 1. Test dels setters

--- src/TestPunt.java
+++ src/TestPunt.java
@@ -15,6 +15,20 @@ public class TestPunt {
         assertEquals(0, p.getX()); // Afirma que la coordenada x val 0
         assertEquals(0, p.getY());
     }
+    @Test
+    public void testSetXCorrect(){
+        Punt p = new Punt();
+        p.setX(2);
+        assertEquals(3, p.getX());
+        assertEquals(2, p.getY());
+    }
+    @Test
+    public void testSetYCorrect(){
+        Punt p = new Punt();
+        p.setY(3);
+        assertEquals(4, p.getY());
+        assertEquals(1, p.getX());
+    }
 
     public static void main(String args[]) {
         org.junit.runner.JUnitCore.main("TestPunt");
